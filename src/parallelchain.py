3b33db3530edb14d6302f9c869fdfc303e17fe73
viveksavita <viveksavita@gmail.com>
2026-02-16 23:47:05 +0000

    completed parallel chain execution

--- src/parallelchain.py
+++ src/parallelchain.py
@@ -1,8 +1,9 @@
 import os
+import asyncio
 from langchain_openai import ChatOpenAI , OpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnablePassthrough, RunnableBranch, Runnable
+from langchain_core.runnables import RunnablePassthrough, RunnableBranch, Runnable, RunnableParallel
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -40,3 +41,34 @@ ChatPromptTemplate.from_messages(
 | llm | StrOutputParser()
 )   
 
+map_chain = RunnableParallel( 
+                             {
+        "summary": summarize_chain,
+        "questions": question_chain,
+        "terms": terms_chain,
+        "topic": RunnablePassthrough(),
+    }
+    )
+
+sysnthesis_prompt = ChatPromptTemplate.from_messages(
+    [
+        ( "system", """ Based on the following information :
+         Summary: {summary}
+         Questions: {questions}
+         Terms: {terms}
+         Sysnthesize a compreshensive answer."""),
+        ("user", "{topic}" )
+    ]
+)
+
+full_chain = map_chain | sysnthesis_prompt | llm | StrOutputParser()
+
+
+async def parallel_chain(topic: str) -> None:
+    result = await full_chain.ainvoke(topic)
+    print("\n Final Synthesis Result: \n", result)
+    
+    
+if __name__ == "__main__":
+    test_topc = "The impact of climate change on global agriculture"
+    asyncio.run(parallel_chain(test_topc))
